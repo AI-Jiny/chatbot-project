04d4c9031f5e49e9223b0694c3ebc4f6958e29d3
AI-Jiny <key140@naver.com>
2023-08-02 16:36:46 +0900

    connecting front-back

--- frontend/frontend_react/.next/static/chunks/webpack.js
+++ frontend/frontend_react/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "36b6d081c4fff7f7"; }
+/******/ 		__webpack_require__.h = function() { return "3ae358d0764143a3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
