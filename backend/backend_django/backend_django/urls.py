68ce801acde4ad22933ebf1436c85c683bd5c856
AI-Jiny <key140@naver.com>
2023-08-03 16:44:23 +0900

    django-rest-framework setting

--- backend/backend_django/backend_django/urls.py
+++ backend/backend_django/backend_django/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('fine_tuning_chatbot.urls')),
+    path('drf/', include('rest_framework.urls')),
 ]
